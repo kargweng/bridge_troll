a15832ef5edb8e1436cabed602b1df597080ba34
Karg Weng <kargweng@gmail.com>
2014-07-15 17:52:01 +0800

    Make seed data available.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ require Rails.root.join('db', 'seeds', 'seed_chapter')
 require Rails.root.join('db', 'seeds', 'admin_user')
 require Rails.root.join('db', 'seeds', 'seed_event')
 
-if Rails.env.development?
-  Seeder::seed_chapter
-  Seeder::admin_user
-  Seeder::seed_event
-end
+# if Rails.env.development?
+Seeder::seed_chapter
+Seeder::admin_user
+Seeder::seed_event
+# end
